40bf3b6ca654fb6a1edc2d554e8a47c2db5c2de2
Steven Personal <stevepopovich8@gmail.com>
2021-05-18 19:45:22 -0400

    Setup a quick and dirty background

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import './App.css';
 import { SamHomeAppBar } from './SamHomeAppBar';
 import { MainFeatureCard } from './MainFeatureCard';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    height: 0,
+    paddingBottom: '200%',
+    backgroundImage: 'url(sam-background-normal.jpg)',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'round',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <>
+    <div className={classes.root}>
       <SamHomeAppBar />
       <MainFeatureCard />
-    </>
+    </div>
   )
 }
 
