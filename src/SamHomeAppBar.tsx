fffadc87fa2dbf5ae23e6849c9f18c58ef4fec65
Steven Personal <stevepopovich8@gmail.com>
2021-05-26 20:49:56 -0400

    Make app bar scroll away

--- src/SamHomeAppBar.tsx
+++ src/SamHomeAppBar.tsx
@@ -1,22 +1,27 @@
-import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, makeStyles, Slide, Toolbar, Typography, useScrollTrigger } from "@material-ui/core";
 import React from "react";
+import { HideOnScroll } from "./HideOnScroll";
 
 const useStyles = makeStyles(() => ({
     appbar: {
-      backgroundColor: 'black'
+      backgroundColor: 'black',
+      height: 72
     },
   }));
 
 export function SamHomeAppBar() {
   const classes = useStyles();
+  const trigger = useScrollTrigger();
 
   return (
-      <AppBar position="static" className={classes.appbar}>
+    <HideOnScroll>
+      <AppBar className={classes.appbar} >
           <Toolbar>
           <Typography variant="h6">
               👋 Hey Sam!
           </Typography>
           </Toolbar>
       </AppBar>
+    </HideOnScroll>
   )
 }
