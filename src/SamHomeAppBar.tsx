186ed63af125164e83d272d2b297922ca8d0747a
Steven Personal <stevepopovich8@gmail.com>
2021-06-18 14:48:57 -0400

    Add under construction banner to app bar

--- src/SamHomeAppBar.tsx
+++ src/SamHomeAppBar.tsx
@@ -2,11 +2,17 @@ import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
 import React from "react";
 import { HideOnScroll } from "./HideOnScroll";
 
+const appBarHeight = 72;
+
 const useStyles = makeStyles(() => ({
     appbar: {
       backgroundColor: 'black',
-      height: 72
+      height: appBarHeight
     },
+    underConstruction: {
+      right: 0,
+      position: 'absolute'
+    }
   }));
 
 export function SamHomeAppBar() {
@@ -19,6 +25,7 @@ export function SamHomeAppBar() {
           <Typography variant="h6">
               👋 Hey Sam!
           </Typography>
+          <img src="/under-construction.png" height={appBarHeight} className={classes.underConstruction}/>
           </Toolbar>
       </AppBar>
     </HideOnScroll>
