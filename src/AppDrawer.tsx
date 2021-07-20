e78526985b030758ed7866ba780dcc5160e7cb39
Steven Personal <stevepopovich8@gmail.com>
2021-07-19 21:58:07 -0400

    fix weird double menu thing visual thing

--- src/AppDrawer.tsx
+++ src/AppDrawer.tsx
@@ -8,7 +8,7 @@ import {useHistory} from 'react-router-dom';
 const drawerWidth = 256;
 const useStyles = makeStyles(() => ({
     drawer: {
-      backgroundColor: accentLighter,
+      backgroundColor: 'transparent',
       width: drawerWidth
     },
     list: {
